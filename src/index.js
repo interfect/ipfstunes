9a2a0149025df8a2df218f847678ef3dcd6a4531
Adam Novak <interfect@gmail.com>
2016-10-16 16:49:49 -0700

    Demo file IO API

--- src/index.js
+++ src/index.js
@@ -14,7 +14,11 @@ var node = new IPFS(temp ? ("tempnode" + randID) : undefined)
 
 console.log(node)
 
-window.node = node
+if(!(typeof window === 'undefined')) {
+    // We are running in the browser and want to expose our IPFS node to browser
+    // code.
+    window.node = node
+}
 
 // Start it up as in https://github.com/ipfs/js-ipfs/blob/c8dbf6ba16017103b29589bb6d4173f954b4325f/src/http-api/index.js
 
@@ -79,23 +83,73 @@ initIfNeeded(node, () => {
             
                 console.log("Loaded")
                 
-                // Add a bootstrap peer so we connect to the signaling server and actually look for peers there.
-                node.bootstrap.add("/libp2p-webrtc-star/ip4/10.1.0.2/tcp/9090/ws/ipfs/QmNQL65n2gcRBmLSfRzEQ5VvBu7cueaic5SLX65AH4GFCP", () => {
+                // Delete this bootstrap peer if already present
+                node.bootstrap.rm("/libp2p-webrtc-star/ip4/10.1.0.2/tcp/9090/ws/ipfs/QmNQL65n2gcRBmLSfRzEQ5VvBu7cueaic5SLX65AH4GFCP", () => {
+                
+                    // Add a bootstrap peer so we connect to the signaling server and actually look for peers there.
+                    node.bootstrap.add("/libp2p-webrtc-star/ip4/10.1.0.2/tcp/9090/ws/ipfs/QmNQL65n2gcRBmLSfRzEQ5VvBu7cueaic5SLX65AH4GFCP", () => {
 
-                    // Go online and connect to things
-                    node.goOnline(() => {
-                        console.log(node.isOnline())
+                        // Go online and connect to things
+                        node.goOnline(() => {
+                            console.log("Online status: ", node.isOnline())
 
-                        //node.swarm.connect("/libp2p-webrtc-star/ip4/10.1.0.2/tcp/9090/ws/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo1", console.log)
+                            //node.swarm.connect("/libp2p-webrtc-star/ip4/10.1.0.2/tcp/9090/ws/ipfs/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSoooo1", console.log)
 
-                        node.swarm.peers((err, peers) => {
-                            peers.forEach((multiaddr) => {
-                                console.log(multiaddr.toString())
+                            // Dump peers we have
+                            node.swarm.peers((err, peers) => {
+                                peers.forEach((multiaddr) => {
+                                    console.log(multiaddr.toString())
+                                })
                             })
-                        })
+                            
+                            
+                            // Add a file
+                            node.files.add(new Buffer("Hello world"), (err, returned) => {
+                                console.log("File add error: ", err)
+                                console.log("File add return: ", returned)
+                                
+                            })
+                            
+                            // Dump the contents
+                            node.files.get("QmNRCQWfgze6AbBCaT1rkrkV5tJ2aP4oTNPb5JZcXYywve", (err, readable) => {
+                                readable.on('data', (ipfs_file_object) => {
+                                    // We get a bunch of file objects
+                                    console.log("File object: ", ipfs_file_object)
+                                    console.log("Data size: ", ipfs_file_object.size)
+                                    
+                                    // They have content streams
+                                    ipfs_file_object.content.on('data', (actual_bytes) => {
+                                        console.log("Data bytes: ", actual_bytes.toString('ascii'))
+                                    })
+                                    
+                                    // But we have to unpause them to actually get data
+                                    ipfs_file_object.content.resume()
+                                })
+                            })
+                            
+                            // We also have the simpler cat API, when not looping over directories.
+                            node.files.cat("QmNRCQWfgze6AbBCaT1rkrkV5tJ2aP4oTNPb5JZcXYywve", (err, content_stream) => {
+                                content_stream.on('data', (actual_bytes) => {
+                                    console.log("Cat bytes: ", actual_bytes.toString('ascii'))
+                                })
+                            })
+                            
+                            // Periodically dump peers
+                            setInterval(() => {
+                                console.log("Peer list:")
+                                node.swarm.peers((err, peers) => {
+                                    peers.forEach((multiaddr) => {
+                                        console.log(multiaddr.toString())
+                                    })
+                                })
+                            }, 60 * 1000); 
+                            
+                            
 
+                        })
                     })
                 })
+                
             })
             
         })
